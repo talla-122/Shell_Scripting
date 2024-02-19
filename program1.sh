d38ab579d2c43a484862556ca0cb0cba9cb8ff47
KALYAN <105667385+talla-122@users.noreply.github.com>
2024-02-19 20:46:20 +0530

    program1.sh

--- program1.sh
+++ program1.sh
@@ -2,9 +2,9 @@
 # print numbers which are divisible by 3 or 5 and not divisible by 15
 
 for i in {1..100}; do
-if (['expr $i%3' == 0] || ['expr $i%5' == 0]) && ['expr $i%15 != 0];
+if ( [ 'expr $i%3' == 0 ] || [ 'expr $i%5' == 0 ] ) && [ 'expr $i%15' != 0 ];
 then
-    echo $i
+    echo "$i"
 fi;
 done
 
