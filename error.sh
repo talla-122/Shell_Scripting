192a2edee6faa7af47522e142b361e509467986b
KALYAN <105667385+talla-122@users.noreply.github.com>
2024-02-19 21:14:40 +0530

    error.sh

--- error.sh
+++ error.sh
@@ -5,3 +5,11 @@ curl logfile url | grep error
 
 # for debugging the shell script
 set -x
+
+
+# what is crontab in linux
+A file containing the schedule of various cron entries that should be run at specified times
+
+# how to open a file in read only mode
+vim -r file1.sh
+
